e14321085b63dcd49e663408afa593d50f4bfb49
Peter Ivanov <nut707@gmail.com>
2022-01-19 02:21:35 +1000

    flight states, view layout container

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,7 +2,22 @@ class Flight < ApplicationRecord
   include AASM
 
   aasm :column => 'state' do
+    state :hangar, initial: true
+    state :on_runway, :in_flight
+
+    event :to_runway do
+      transitions from: :hangar, to: :on_runway
+    end
+
+    event :take_off do
+      transitions from: :on_runway, to: :in_flight
+    end
+
+    event :park do
+      transitions from: [:on_runway, :in_flight], to: :hangar
+    end
+
   end
 
-  
+
 end
